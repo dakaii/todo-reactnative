9e05c0b38ecb9cb8042017f048291e0810b759e7
dakaii <daiki815@gmail.com>
2018-01-05 12:27:12 +0900

    added styles

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, TextInput, Button} from 'react-native';
 
 class App extends React.Component {
+  state = {
+    text: "",
+    todo: []
+  }
+
+  addTodo = () =>{
+    var arr = this.state.todo
+    arr.push(this.state.text);
+    this.setState({todo: arr, text: ""})
+  }
+
+  deleteTodo = (index) => {
+    var arr = this.state.todo;
+    arr.splice(index, 1);
+    this.setState({todo: arr});
+  }
+
+  renderTodos = () => {
+    return this.state.todo.map((value, index) => {
+      return (
+        <Text
+          key={index}
+          onPress={() => {this.deleteTodo(index)}}>
+          {value}
+        </Text>
+      )
+    })
+  }
+
   render() {
     return(
       <View style={styles.viewStyle}>
-        <text>Hello World, this app is great.</text>
+        <Text style={styles.title}>Todo App</Text>
+        <TextInput
+          style={styles.inputStyle}
+          onChangeText={(text)=>this.setState({text})}
+          value={this.state.text}
+        />
+        <Button
+          title="Add Todo"
+          color="green"
+          onPress={this.addTodo}
+        />
+        {this.renderTodos()}
       </View>
     )
   }
@@ -15,7 +55,19 @@ const styles = {
   viewStyle: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    margin: 10
+  },
+  inputStyle: {
+    height: 40,
+    alignSelf: "stretch",
+    borderColor: "green",
+    borderWidth: 1
+  },
+  title: {
+    fontSize: 30,
+    color: 'green',
+    fontWeight: 'bold'
   }
 }
 
